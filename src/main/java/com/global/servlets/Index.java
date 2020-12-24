a3c4185c7daec7ead27711a632833a2c49491964
Julien Wiegandt <wiegandtjulien2@gmail.com>
2020-12-24 22:59:29 +0100

    first custom pages done

--- src/main/java/com/global/servlets/Index.java
+++ src/main/java/com/global/servlets/Index.java
@@ -1,11 +1,11 @@
-package com.global.ToDo_JEE;
+package com.global.servlets;
 
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+public class Index extends HttpServlet {
     private String message;
 
     public void init() {
@@ -13,13 +13,7 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
     }
 
     public void destroy() {
---
+++ src/main/webapp/menu.jsp
@@ -0,0 +1,14 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wiega
+  Date: 24/12/2020
+  Time: 22:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<ul>
+    <li>Home</li>
+    <li>My ToDo</li>
+    <li>About Us</li>
+    <li>Settings</li>
+</ul>
